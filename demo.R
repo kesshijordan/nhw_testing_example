6f413a14d4fdf25a8443bf5d0651a29a44b96c52
kesshijordan <Kesshi.Jordan@ucsf.edu>
2016-09-05 18:14:22 -0700

    assertion test added

--- demo.R
+++ demo.R
@@ -1,7 +1,12 @@
 args <- commandArgs(trailingOnly = TRUE)
 demographics <- read.csv(file='participants.tsv', head=TRUE, sep="\t")
-age <- demographics[4]
+age <- demographics[2]
 demean_age <- age - sum(age)/length(age)
+
+mean_age <- sum(age)/length(age)
+
+stopifnot(mean_age < 100)
+stopifnot(mean_age > 0)
 
 write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
 
