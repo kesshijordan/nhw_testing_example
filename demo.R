7411b782f0edc9289d2165e6a53e4e8d604c6e91
kesshijordan <Kesshi.Jordan@ucsf.edu>
2016-09-05 17:53:26 -0700

    brokeit

--- demo.R
+++ demo.R
@@ -1,8 +1,7 @@
 demographics <- read.csv(file='participants.tsv', head=TRUE, sep="\t")
 age <- demographics[4]
-demean_age <- age - sum(age)/length(age)
-
+#demean_age <- age - sum(age)/length(age)
+demean_age <- age/0
 write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
 
 print("done!")
-
