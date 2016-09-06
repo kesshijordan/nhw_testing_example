68abbe7682f16aa0a6597f04a95c88959508c8ab
kesshijordan <Kesshi.Jordan@ucsf.edu>
2016-09-05 18:07:11 -0700

    added step

--- demo.R
+++ demo.R
@@ -1,7 +1,8 @@
+args <- commandArgs(trailingOnly = TRUE)
 demographics <- read.csv(file='participants.tsv', head=TRUE, sep="\t")
 age <- demographics[4]
-#demean_age <- age - sum(age)/length(age)
-demean_age <- age/0
+demean_age <- age - sum(age)/length(age)
+
 write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
 
 print("done!")
